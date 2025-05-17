486b90af67111d22d5452d1fb54f69f6134dfb49
julianv2912 <july2villa9@gmail.com>
2025-05-17 01:22:31 +0000

    Actualizacion a archivo de administrador

--- proyecto/src/main.js
+++ proyecto/src/main.js
@@ -1,6 +1,6 @@
 import { supabase } from "./supabase.js";
 import { mostrarLogin } from "./login.js";
-import { mostrarRegistro } from "./registro.js";
+import { mostrarDatos } from "./usuario.js";
 
 document.addEventListener('DOMContentLoaded', async () => {
 const user = await validarSesion();
@@ -302,3 +302,4 @@ window.actualizarIconoFavorito = actualizarIconoFavorito;
 window.mostrarFavoritos = mostrarFavoritos;
 window.mostrarAleatorio = mostrarAleatorio;
 window.mostrarAlbum = mostrarAlbum;
+window.mostrarDatos = mostrarDatos

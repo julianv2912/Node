cc57746501a0aeb70526e14cc3bfc30231998124
julianv2912 <july2villa9@gmail.com>
2025-05-17 00:49:17 +0000

    Vinculacion de supabase al codigo

--- proyecto/src/supabase.js
+++ proyecto/src/supabase.js
@@ -1,6 +1,6 @@
 // src/supabase.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'aqui la url de supabase';
-const supabaseKey = 'aqui el codigo de supabase';
+const supabaseUrl = 'https://nkgougnngibklwowicwy.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5rZ291Z25uZ2lia2x3b3dpY3d5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc0MzkxMTMsImV4cCI6MjA2MzAxNTExM30.AugXHvwGUqy0L3xgld17nkLmTNh3o0IaRwqbeLDfs8o';
 export const supabase = createClient(supabaseUrl, supabaseKey);

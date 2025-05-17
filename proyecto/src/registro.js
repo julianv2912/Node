1fb240bcb2f9c39c94f5f7f6ad3513dfc20d0280
julianv2912 <july2villa9@gmail.com>
2025-05-17 01:05:19 +0000

    Verificacion de login

--- proyecto/src/registro.js
+++ proyecto/src/registro.js
@@ -1,6 +1,5 @@
 import { supabase } from './supabase.js';
-import { mostrarLogin } from './login.js'; // Para volver al login luego del
-registro
+import { mostrarLogin } from './login.js'; // Para volver al login luego del registro
 
 export function mostrarRegistro() {
 document.getElementById('app').innerHTML = `
